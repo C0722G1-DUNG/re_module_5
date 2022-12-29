3bcaacb9abb46fe2f30db62707168483a4422cba
CO722G1-NguyenDuyDung <duydungne10@gmail.com>
2022-12-29 19:29:44 +0700

    n

--- case_study/component/component-furama/component-furama/src/app/contract/create-contract/create-contract.component.ts
+++ case_study/component/component-furama/component-furama/src/app/contract/create-contract/create-contract.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-contract',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-contract.component.css']
 })
 export class CreateContractComponent implements OnInit {
+  formContractCreate: FormGroup= new FormGroup({});
 
-  constructor() { }
+  constructor() {
+    this.formContractCreate =new FormGroup(
+      {
+        id:new FormControl(),
+        startDate:new FormControl('',Validators.required),
+        endDate:new FormControl('',Validators.required),
+        deposit:new FormControl('',Validators.required),
+        customer:new FormControl('',Validators.required),
+        facility:new FormControl('',Validators.required),
+        employee:new FormControl('',Validators.required),
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
 
+  saveContract() {
+
+  }
 }
